e1acfd0722aa78d9c43ec64d372f57b03864d270
ali <bcsf19m513@pucit.edu.pk>
2020-12-04 18:57:46 +0500

    All Done

---
+++ Lab10/Task03/Task03.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+int getinput(){
+    char buf[10];
+    int rv = read(0, buf, sizeof(buf));
+    printf("\nNumber of bytes read are %d\n", rv);
+    return 0;
+}
+int main(){
+    getinput();
+    return 0;
+}
