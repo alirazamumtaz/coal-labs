734d73853b7985fd2b5618cf525cc3e547ab5648
Your Name <bcsf19m513@pucit.edu.pk>
2021-01-15 06:28:41 +0000

    All task added

---
+++ Lab13/Task2.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+void f1(char* data){
+	char buff[10];
+	strcpy(buff, data);
+	printf("%s\n", buff);
+	return;
+}
+int foo(){
+	printf("Learning is fun with Arif Butt.\n");
+	exit(0);
+}
+int main(int argc, char*argv[]){
+	f1(argv[1]);
+	exit(0);
+}
